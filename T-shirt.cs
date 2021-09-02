2bc91764e5f24e02031e5b764b86517b39d86d12
0996937288an <khathawut744@gmail.com>
2021-09-02 23:46:47 +0700

    Revert "Revert "no message""
    This reverts commit 8228fe2d2bab18277b3090cecd5ea5c8276ab15c.

---
+++ T-shirt.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+class Tshirt
+{
+    public string size;
+
+    public float price;
+
+    public string image;
+
+    public Tshirt(string valueSize, float valuePrice ,string valueImage){
+
+        size = valueSize;
+        price = valuePrice;
+        image = valueImage;
+
+    }
+
+
+
+
+}
